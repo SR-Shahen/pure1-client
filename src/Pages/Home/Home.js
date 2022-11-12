a5fa14749faa909a9b06cc9fbf4c7b70ca5eeb68
SR-Shahen <s.r.shahenda@gmail.com>
2022-11-12 13:35:52 +0600

    project intialize

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Home = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold text-purple-600'>Welcome to PURE 1</h1>
+        </div>
+    );
+};
+
+export default Home;
